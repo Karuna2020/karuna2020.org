c87e501896f44f149171dea0c668629c585a2c81
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-29 00:20:02 +0530

    :sparkles: Add footer

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -1,7 +1,18 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const Footer = () => <></>
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <p>© {new Date().getFullYear()} Karuna 2020</p>
+      <nav>
+        <a href="/">Home</a>
+        <a href="https://open-data.karuna2020.org">API</a>
+        <a href="https://github.com/Karuna2020">GitHub</a>
+      </nav>
+    </div>
+  </footer>
+)
 
 Footer.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,108 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { SEO, Image } from "../components/elements"
+import { Footer } from "../components/sections"
 
 import "../styles/common.scss"
 import "../styles/index.scss"
 
-const IndexPage = () => (
-  <>
-    <SEO title="Karuna 2020" />
-    <main className="home-main">
-      <Image
-        className="logo"
-        alt="Karuna 2020"
-        src="https://cdn.karuna2020.org/logo-vertical.svg"
-      />
-      <p>
-        While many of us will be able to comfortably sustain for months at home,
-        there will be millions of people—especially those with limited financial
-        means, senior citizens, the differently abled, etc.—who may find it very
-        difficult to pull through even for a few days without the basic three
-        meals or essential medicines.
-      </p>
-      <p>
-        While the Government is putting significant effort into making the lives
-        of its citizens comfortable, what can we do to help?{" "}
-        <strong>Karuna 2020</strong> is a small effort to try to bring together
-        people and do our part in helping feed those most affected during the
-        coronavirus pandemic.
-      </p>
-      <div className="buttons">
-        <a
-          href="https://www.facebook.com/Karuna2020.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Follow our progress on Facebook &rarr;
-        </a>
-        <a
-          href="https://github.com/Karuna2020"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contribute on GitHub
-        </a>
-      </div>
-      <section className="numbers">
-        <h1>Karuna 2020 in numbers</h1>
-        <div className="data">
-          <div>
-            <div>₹2390</div>
-            <div>Total raised</div>
-          </div>
-          <div>
-            <div>0</div>
-            <div>Kits delivered</div>
-          </div>
-          <div>
-            <div>0</div>
-            <div>People impacted</div>
-          </div>
-        </div>
+const IndexPage = () => {
+  const [totalRaised, setTotalRaised] = useState(0)
+  const [contributors, setContributors] = useState(0)
+  const [volunteers, setVolunteers] = useState(0)
+
+  useEffect(() => {
+    fetch("https://open-data.karuna2020.org/summary.json")
+      .then(response => response.json())
+      .then(json => {
+        if (json.totalAmountRaised) setTotalRaised(json.totalAmountRaised)
+        if (json.numberOfContributors)
+          setContributors(json.numberOfContributors)
+        if (json.numberOfVolunteers) setVolunteers(json.numberOfVolunteers)
+      })
+      .catch(() => {})
+  }, [])
+
+  return (
+    <>
+      <SEO title="Karuna 2020" />
+      <main className="container home-main">
+        <Image
+          className="logo"
+          alt="Karuna 2020"
+          src="https://cdn.karuna2020.org/logo-vertical.svg"
+        />
         <p>
-          We've made all of our data, including contributions, procurement, and
-          beneficiaries,{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/Karuna2020"
-          >
-            open source
-          </a>{" "}
-          on GitHub. You can use our{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://open-data.karuna2020.org"
-          >
-            API
-          </a>{" "}
-          or contribute to our{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/Karuna2020/open-data"
-          >
-            Open data
-          </a>{" "}
-          platform.
+          While many of us will be able to comfortably sustain for months at
+          home, there will be millions of people—especially those with limited
+          financial means, senior citizens, the differently abled, etc.—who may
+          find it very difficult to pull through even for a few days without the
+          basic three meals or essential medicines.
         </p>
-      </section>
-    </main>
-  </>
-)
+        <p>
+          While the Government is putting significant effort into making the
+          lives of its citizens comfortable, what can we do to help?{" "}
+          <strong>Karuna 2020</strong> is a small effort to try to bring
+          together people and do our part in helping feed those most affected
+          during the coronavirus pandemic.
+        </p>
+        <div className="buttons">
+          <a
+            href="https://www.facebook.com/Karuna2020.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Follow our progress on Facebook &rarr;
+          </a>
+        </div>
+        <section className="numbers">
+          <h1>Karuna 2020 in numbers</h1>
+          <div className="data">
+            <div>
+              <div>₹{totalRaised.toLocaleString()}</div>
+              <div>Total raised</div>
+            </div>
+            <div>
+              <div>{contributors.toLocaleString()}</div>
+              <div>Contributors</div>
+            </div>
+            <div>
+              <div>{volunteers.toLocaleString()}</div>
+              <div>Volunteers</div>
+            </div>
+          </div>
+          <p>
+            We've made all of our data, including contributions, procurement,
+            and beneficiaries,{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/Karuna2020"
+            >
+              open source
+            </a>{" "}
+            on GitHub. You can use our{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://open-data.karuna2020.org"
+            >
+              API
+            </a>{" "}
+            or contribute to our{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/Karuna2020/open-data"
+            >
+              Open data
+            </a>{" "}
+            platform.
+          </p>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
 
 export default IndexPage
