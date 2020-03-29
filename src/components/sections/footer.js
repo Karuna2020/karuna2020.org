6b8cbdc02c58c92ec25dbdd96220d3cbe1ed3f52
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-29 15:08:10 +0530

    :sparkles: Add links to footer

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
 
 const Footer = () => (
   <footer>
     <div className="container">
       <p>© {new Date().getFullYear()} Karuna 2020</p>
       <nav>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
+        <Link to="/volunteers">Volunteers</Link>
+        <Link to="/contribute">Contribute</Link>
         <a href="https://open-data.karuna2020.org">API</a>
         <a href="https://github.com/Karuna2020">GitHub</a>
       </nav>
