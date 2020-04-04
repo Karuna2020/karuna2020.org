2439efd67752dda7d1510916c6ea6463f6126571
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-05 04:51:32 +0530

    :fire: Remove legacy site assets

--- src/components/elements.js
+++
@@ -1,2 +0,0 @@
-export { default as Image } from "./elements/image"
-export { default as SEO } from "./elements/seo"
--- src/components/sections.js
+++
@@ -1,2 +0,0 @@
-export { default as Header } from "./sections/header.js"
-export { default as Footer } from "./sections/footer.js"
