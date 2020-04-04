fafc7407e7818cfa43a8515ae81d6e9a515b4ec6
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-05 03:34:05 +0530

    :sparkles: Copy open data files

--- open-data.js
+++ open-data.js
@@ -1,1 +1,14 @@
+const fs = require('fs')
+const { join } = require('path')
+
 console.log('Setting up open data')
+
+fs.mkdirSync(join('.', 'src', 'data', 'open-data'), { recursive: true })
+fs.copyFileSync(
+    join('.', 'open-data', 'summary.json'),
+    join('.', 'src', 'data', 'open-data', 'summary.json')
+)
+fs.copyFileSync(
+    join('.', 'open-data', 'amount-received.json'),
+    join('.', 'src', 'data', 'open-data', 'amount-received.json')
+)
