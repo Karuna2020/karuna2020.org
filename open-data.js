4001d5bf1c84b99ce75dc6e937300ed80ed94180
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-05 03:39:59 +0530

    :truck: Rename open-data to data

--- open-data.js
+++ open-data.js
@@ -6,9 +6,9 @@ console.log('Setting up open data')
 fs.mkdirSync(join('.', 'src', 'data', 'open-data'), { recursive: true })
 fs.copyFileSync(
     join('.', 'open-data', 'summary.json'),
-    join('.', 'src', 'data', 'open-data', 'summary.json')
+    join('.', 'src', 'data', 'data', 'summary.json')
 )
 fs.copyFileSync(
     join('.', 'open-data', 'amount-received.json'),
-    join('.', 'src', 'data', 'open-data', 'amount-received.json')
+    join('.', 'src', 'data', 'data', 'amount-received.json')
 )
