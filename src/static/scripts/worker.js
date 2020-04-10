5dec3b0386650230ae00a980282551665cd02adc
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-11 01:20:47 +0530

    :fire: Remove offline, worker

--- src/static/scripts/worker.js
+++
@@ -1,7 +0,0 @@
-// this file will be imported into the service worker
-
-self.addEventListener('message', function(event) {
-    if (event.data.action === 'skipWaiting') {
-        self.skipWaiting()
-    }
-})
